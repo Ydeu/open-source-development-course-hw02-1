9dbb3ae2b70e3259f41bdc5217c2b2b5a8dd27c9
Ydeu <sukurma98@gmail.com>
2020-01-28 19:09:49 +0100

    Added more tests : vct_test.py

--- ossdev/tests/vct_test.py
+++ ossdev/tests/vct_test.py
@@ -50,8 +50,12 @@ class VectorTest(unittest.TestCase):
 
     def test_mul(self):
         a = Vector([2, 4, 8])
+        b = Vector([1, 2, 4])
+        c = Vector([2, 2, 2])
 
         self.assertEqual((a * 0.5).get(), [1, 2, 4])
+        self.assertEqual((a * b).get(), [2, 8, 32])
+        self.assertEqual(c.transpose() * b, 14)
 
     def test_xor(self):
         a = Vector([2, 2, 1])
@@ -64,17 +68,33 @@ class VectorTest(unittest.TestCase):
 
         self.assertEqual(a.length(), math.sqrt(20))
         self.assertEqual(b.length(), math.sqrt(18))
+        self.assertAlmostEqual(Vector([2, 4]).length(), 4.47213595499, 3)
+        self.assertAlmostEqual(Vector([2, 4, 5]).length(), 6.708203932499369, 3)
+        self.assertEqual(Vector([]).length(), 0)
 
     def test_dot(self):
         a = Vector([1, 2])
         b = Vector([2, -1])
+
         self.assertEqual(a.dot(b), 0)
 
-    def test_length(self):
-        # Uncomment after passing
-        # self.assertAlmostEqual(Vector([2, 4]).length(), 4.47213595499, 3)
-        # self.assertAlmostEqual(Vector([2, 4, 5]).length(), 6.708203932499369, 3)
-        return
+    def test_and(self):
+        a = Vector([2, 2, 2])
+        b = Vector([1, 1, 1])
+        c = 3
+
+        self.assertEqual(a.__and__(b).get(), [0, 0, 0])
+        self.assertEqual(a.__and__(c).get(), [2, 2, 2])
+        self.assertEqual(b.__and__(a).get(), [0, 0, 0])
+
+    def test_setitem(self):
+        a = Vector([2, 3, 5])
+        first_element = a[0]
+
+        a.__setitem__(0, 10.752)
+        self.assertEqual(a[0], first_element)
+        a.__setitem__(0, "meh")
+        self.assertEqual(a[0], first_element)
 
 
 if __name__ == "__main__":
